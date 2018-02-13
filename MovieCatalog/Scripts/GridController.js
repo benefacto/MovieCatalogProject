da1493de9b14d50553621963f41416d25b4698bc
Ben Walker <ben.walker18@gmail.com>
2018-02-13 11:12:08 -0800

    got parsing & rendering on ui-grid data from get request working

--- MovieCatalog/Scripts/GridController.js
+++ MovieCatalog/Scripts/GridController.js
@@ -1,35 +1,39 @@
 ﻿var GridController = function ($scope, $http) {
-    // TO-DO: Make GET request to service to obtain data
+    var endpoint = "http://localhost:51893/Service.svc/movie";
+
     var init = function () {
-        $http.get('http://localhost:51893/Service.svc/movie')
-            .then(function (data) {
-                console.log(JSON.parse(data));
-                return JSON.parse(data);
-            }, function (data) {
+        $http.get(endpoint)
+            .then(function (response) {
+                $scope.gridOptions.data = JSON.parse(response.data.GetMoviesResult);
+            }, function (response) {
+                alert("error!");
+            });
+    }
+
+    var update = function () {
+        // TO-DO: Get PUT request to service to update data working
+        $http.put(endpoint + "/" + JSON.stringify(rowEntity))
+            .then(function (response) {
+                console.log(JSON.parse(response.data.GetMoviesResult));
+            }, function (response) {
                 alert("error!");
             });
     }
 
     $scope.gridOptions = {
         enableCellEdit: true,
-        data: init(),
         onRegisterApi: function (gridApi) {
-            $scope.gridApi = gridApi;
             gridApi.edit.on.afterCellEdit($scope, function (rowEntity, colDef, newValue, oldValue) {
                 console.log("afterCellEdit fired");
                 if (newValue !== oldValue && $scope.recentEditedValue !== newValue) {
-                    // TO-DO: Get PUT request to service to update data working
-                    $http.put('http://localhost:51893/Service.svc/movie/' +
-                        JSON.stringify(rowEntity))
-                        .then(function (data) {
-                            return JSON.parse(data.GetMoviesResult);
-                        }, function (data) {
-                            alert("error!");
-                        });
+                    update();
+                    init();
                 }
             });
-        }
+        },
     }
+
+    init();
 };
 
 GridController.$inject = ['$scope', '$http'];
