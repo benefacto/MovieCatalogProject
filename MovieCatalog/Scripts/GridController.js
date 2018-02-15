51b91ece7d42c692af33b8d6cdae9be48c886722
Ben Walker <ben.walker18@gmail.com>
2018-02-15 12:12:22 -0800

    disable id edits, various front-end refactoring

--- MovieCatalog/Scripts/GridController.js
+++ MovieCatalog/Scripts/GridController.js
@@ -11,25 +11,39 @@
     }
 
     var update = function (rowEntity) {
-        // TO-DO: Get PUT request to service to update data working
         var newMovie = rowEntity;
         var data = { "movieJson" : rowEntity };
         $http.put(endpoint, angular.toJson(data))
             .then(function (response) {
-                console.log(angular.fromJson(response.data));
+                init();
             }, function (response) {
                 alert("error!");
             });
     }
 
     $scope.gridOptions = {
-        enableCellEdit: true,
+        columnDefs: [
+            {
+                name: 'id', enableCellEdit: false,
+            },
+            {
+                name: 'title', enableCellEdit: true,
+            },
+            {
+                name: 'director', enableCellEdit: true,
+            },
+            {
+                name: 'year', enableCellEdit: true,
+            },
+            {
+                name: 'runningTime', enableCellEdit: true,
+            }
+        ],
         onRegisterApi: function (gridApi) {
             gridApi.edit.on.afterCellEdit($scope, function (rowEntity, colDef, newValue, oldValue) {
                 console.log("afterCellEdit fired");
                 if (newValue !== oldValue && $scope.recentEditedValue !== newValue) {
                     update(rowEntity);
-                    //init();
                 }
             });
         },
